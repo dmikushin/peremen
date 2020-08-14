adf27e21b0b37b6725f55d556d85d6d0c58432e3
Dmitry Mikushin <dmitry@kernelgen.org>
2020-08-15 01:51:01 +0200

    Adding a simple player

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import peremen from './peremen.mp3';
+import './App.css';
+
+function App() {
+  function play() {
+    document.getElementById('player').play();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <audio id="player" src={peremen}></audio>
+        <a onClick={play}><img src={logo} className="App-logo" alt="logo" /></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
