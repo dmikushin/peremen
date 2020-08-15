ee6f5276bc7d1cdd5b7f0224deebcc4bd5913933
Dmitry Mikushin <dmitry@kernelgen.org>
2020-08-15 03:17:29 +0200

    Updates

---
+++ precache-manifest.268074661e37729e6c052335111fd719.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf39978d3456a1c6bc1e0c299deb3bc",
+    "url": "/peremen/index.html"
+  },
+  {
+    "revision": "7097a38b49709e282875",
+    "url": "/peremen/static/css/main.cf112675.chunk.css"
+  },
+  {
+    "revision": "439545251a2808677e29",
+    "url": "/peremen/static/js/2.f32ca2e5.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/peremen/static/js/2.f32ca2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7097a38b49709e282875",
+    "url": "/peremen/static/js/main.567213bb.chunk.js"
+  },
+  {
+    "revision": "eae5a675ee92ac757d84",
+    "url": "/peremen/static/js/runtime-main.348e5f8a.js"
+  },
+  {
+    "revision": "cf0260edda156c7c169bec39a8df5fcf",
+    "url": "/peremen/static/media/background.cf0260ed.jpg"
+  },
+  {
+    "revision": "638d7f788efa0834f2d4fafe6d7fcff4",
+    "url": "/peremen/static/media/logo.638d7f78.png"
+  },
+  {
+    "revision": "941e46bf8d2869d5328b5e196447b59d",
+    "url": "/peremen/static/media/peremen.941e46bf.mp3"
+  }
+]);
