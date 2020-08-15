ff89411cc630cfe521bf17a4144c22330981f139
Dmitry Mikushin <dmitry@kernelgen.org>
2020-08-15 02:51:41 +0200

    Testing ntp-time-sync, which does not work yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import NtpTimeSync from 'ntp-time-sync';
 import logo from './logo.png';
 import peremen from './peremen.mp3';
 import './App.css';
 
 function App() {
   function play() {
+    const timeSync = NtpTimeSync.getInstance();
+ 
+    timeSync.getTime().then(function (result) {
+      console.log("current system time", new Date());
+      console.log("real time", result.now);
+      console.log("offset in milliseconds", result.offset);
+    });
+
     document.getElementById('player').play();
   }
 
